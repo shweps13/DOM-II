46c4c01d4d4c351bb9eea77179953100f11a19ed
Heorhii Siburov <g.siburov@gmail.com>
2019-08-06 13:32:11 -0700

    mouseover and keydown added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+
+// Mouseover event
+let nav = document.getElementById("nav");
+//   console.log(nav)
+nav.addEventListener("mouseenter", function(event) {   
+  event.target.style.color = "purple";
+
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+
+nav.addEventListener("mouseover", function(event) {   
+  event.target.style.color = "orange";
+
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 1000);
+}, false);
+
+
+
+// Keydown event
+const input = document.querySelector("input");
+const log = document.getElementById("log");
+
+input.addEventListener("keydown", logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
+}
